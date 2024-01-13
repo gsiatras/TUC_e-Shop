c812f9a968ffd7f566746c6fa10440cc3cfdeebf
gsiatras <gsiatras@isc.tuc.gr>
2024-01-13 15:11:38 +0200

    back to working loccaly

--- order-service/src/controllers/service.js
+++ order-service/src/controllers/service.js
@@ -27,12 +27,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Middleware to handle CORS if needed
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 
 // Your route handling
