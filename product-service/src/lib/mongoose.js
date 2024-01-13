ff98891b8341eea7657499ca46a4d6e0ea28b5ee
gsiatras <gsiatras@isc.tuc.gr>
2024-01-13 18:15:02 +0200

    back to working loccaly

--- product-service/src/lib/mongoose.js
+++ product-service/src/lib/mongoose.js
@@ -4,7 +4,7 @@ export function mongooseConnect() {
     if (mongoose.connection.readyState === 1) {
         return mongoose.connection.asPromise();
     } else {
-        const url = 'mongodb://adm1n:Pa55w0rd@172.17.0.1:27017/test';
+        const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/test';
         return mongoose.connect(url);
     }
-}
+} 
