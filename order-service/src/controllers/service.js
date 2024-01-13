9da691764169bdfec5cd513150e695a23645632d
gsiatras <gsiatras@isc.tuc.gr>
2024-01-13 15:34:38 +0200

    back to working loccaly

--- order-service/src/controllers/service.js
+++ order-service/src/controllers/service.js
@@ -13,17 +13,17 @@ import { decodeJwt } from '../utils/jwtUtils.js';
 const app = express();
 const PORT = 3007; // You can use any port you prefer
 const stripe = stripePackage(process.env.STRIPE_SECRET_KEY || "sk_test_51OU5hwJvUVrQpHMkIU7tDBk3WAxZjVw0bNMhuMPy9w1a6iB5V9MR6Cr1zUYLvTILHSmMh9P73ZPf20SpLexSlOtO00npckONT4");
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+
   
 app.use(cors({
     origin: 'http://34.116.170.68:3001',  // Update with the origin of your frontend app
