80ac59b70b875d60b7962b789b33cea878239da1
gsiatras <gsiatras@isc.tuc.gr>
2024-01-13 15:20:06 +0200

    back to working loccaly

--- Frontend/pages/seller/orders.js
+++ Frontend/pages/seller/orders.js
@@ -47,11 +47,7 @@ export default function OrdersPage() {
             const newStatus = selectedStatuses[orderId];
             //console.log(newStatus);
             // Make an axios request to update the order on the server
-            axios.put('http://34.116.170.68:3007/orders?orderId=' + orderId, { status: newStatus }, {headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                // Add any other required headers
-            },})
+            axios.put('http://34.116.170.68:3007/orders?orderId=' + orderId, { status: newStatus },)
                 .then(response => {
                     // console.log(`Order ${orderId} updated successfully`);
                 })
