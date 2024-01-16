da7928427405fd9e34eb10cf81d65d5c909eb9c8
gsiatras <gsiatras@isc.tuc.gr>
2024-01-16 13:53:50 +0200

    sad

--- Frontend/middleware.js
+++ Frontend/middleware.js
@@ -4,27 +4,19 @@ import { decodeJwt } from './utils/jwtUtils';
 export async function middleware(req) {
   try {
     let rl = null;
-    let accessToken = null; // Declare outside the if block
+    
 
     if (req.cookies) {
-      accessToken = req.cookies?.get('access_token')?.value;
+      rl = req.cookies?.get('role')?.value;
       //console.log(accessToken);
-
-      if (accessToken) {
-        //console.log(accessToken);
-        const decodedToken = await decodeJwt(accessToken);
-        const { preferred_username: uname, email, realm_access: { roles } } = decodedToken;
-        rl = roles.includes("Customer") ? "Customer" : roles.includes("Seller") ? "Seller" : null;
-      }
     }
 
-    const url = req.url;
-    const absoluteHomeUrl = new URL('/', req.nextUrl);
+    
 
-    if ((!accessToken && url.includes('/seller')) || (accessToken && rl !== 'Seller' && url.includes('/seller'))) {
+    if ((!rl && url.includes('/seller')) || (rl !== 'Seller' && url.includes('/seller'))) {
       //console.log("Redirecting to home URL due to seller conditions"); // Add this line for debugging
       return NextResponse.redirect(absoluteHomeUrl);
-    } else if ((!accessToken && url.includes('/customer')) || (accessToken && rl !== 'Customer' && url.includes('/customer'))) {
+    } else if ((!rl && url.includes('/customer')) || (rl !== 'Customer' && url.includes('/customer'))) {
       //console.log("Redirecting to home URL due to customer conditions"); // Add this line for debugging
       return NextResponse.redirect(absoluteHomeUrl);
     }
